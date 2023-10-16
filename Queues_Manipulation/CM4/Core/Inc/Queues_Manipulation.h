5bb97278acd92a4e4116ec4f96373e2f391aa5e7
oussemajelassi <oussjls57@gmail.com>
2023-10-16 15:18:29 +0100

    Added CM4 Folder

---
+++ Queues_Manipulation/CM4/Core/Inc/Queues_Manipulation.h
@@ -0,0 +1,14 @@
+/*
+ * Queues_Manipulation.h
+ *
+ *  Created on: Oct 16, 2023
+ *      Author: ousjl
+ */
+
+#ifndef INC_QUEUES_MANIPULATION_H_
+#define INC_QUEUES_MANIPULATION_H_
+
+
+
+
+#endif /* INC_QUEUES_MANIPULATION_H_ */
