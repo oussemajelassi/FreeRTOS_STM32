266837b6b4bc792261f33330b0ed8622ab4edd9e
oussemajelassi <oussjls57@gmail.com>
2023-10-17 16:21:45 +0100

    Adress Bugs

--- Shared_Ressources/CM4/Core/Src/SharedRessources.cpp
+++ Shared_Ressources/CM4/Core/Src/SharedRessources.cpp
@@ -8,9 +8,10 @@
 #include "SharedRessources.h"
 #include "stm32h7xx.h"
 
-/*** Static Attribute ***/
+/*** Static Attributes ***/
 
 uint8_t Producer::sProducedId_u8 = 0 ;
+uint8_t Consumer::sConsumerID_u8 = 0 ;
 
 /*** Methods Producer **/
 
@@ -26,9 +27,29 @@ Producer::Producer(GPIO_TypeDef *GPIOx , uint16_t GPIO_Pin)
 Producer::~Producer(void)
 {
 	HAL_GPIO_WritePin(this->GPIOx , this->GPIO_Pin , GPIO_PIN_RESET ) ;
+	this->sProducedId_u8 -- ;
 }
 
-void Producer::InsertItem( CircularBuffer buff )
+void Producer::InsertItem( CircularBuffer * buff )
 {
-	buff.insert_item(this->ProductId_u8) ;
+	buff->insert_item(this->ProductId_u8) ;
 }
+
+/*** Methods Consumer **/
+
+Consumer::Consumer(void)
+{
+	this->ConsumerID_u8 =  this->sConsumerID_u8 ;
+	this->sConsumerID_u8 ++ ;
+}
+
+Consumer::~Consumer()
+{
+	this->sConsumerID_u8 -- ;
+}
+
+uint8_t Consumer::RetrieveItem(CircularBuffer* buff)
+{
+	return  ( buff->read_item() ) ;
+}
+
