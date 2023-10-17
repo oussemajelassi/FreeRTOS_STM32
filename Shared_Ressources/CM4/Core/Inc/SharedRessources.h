439ced84b4310d1b26ffc150ea780c37804281c1
oussemajelassi <oussjls57@gmail.com>
2023-10-17 14:58:18 +0100

    Added Producer class

--- Shared_Ressources/CM4/Core/Inc/SharedRessources.h
+++ Shared_Ressources/CM4/Core/Inc/SharedRessources.h
@@ -8,6 +8,31 @@
 #ifndef INC_SHAREDRESSOURCES_H_
 #define INC_SHAREDRESSOURCES_H_
 
+#include "CircularBuffer.h"
+#include "stm32h7xx.h"
+class Producer
+{
+private :
+	uint16_t GPIO_Pin ;
+	GPIO_TypeDef * GPIOx ;
+	uint8_t ProductId_u8 ;
+public :
+	static uint8_t sProducedId_u8  ;
+	Producer(GPIO_TypeDef *GPIOx , uint16_t GPIO_Pin)  ;
+	~Producer(void) ;
+	void InsertItem ( CircularBuffer buff ) ;
+
+};
+
+class Consumer
+{
+private :
+	uint8_t NeededQuantity_8 ;
+public :
+	Consumer(void) ;
+	~Consumer(void);
+	uint8_t RetrieveItem ( CircularBuffer buff ) ;
+};
 
 
 #endif /* INC_SHAREDRESSOURCES_H_ */
--- Shared_Ressources/CM4/Core/Src/SharedRessources.cpp
+++ Shared_Ressources/CM4/Core/Src/SharedRessources.cpp
@@ -5,4 +5,30 @@
  *      Author: ousjl
  */
 
+#include "SharedRessources.h"
+#include "stm32h7xx.h"
 
+/*** Static Attribute ***/
+
+uint8_t Producer::sProducedId_u8 = 0 ;
+
+/*** Methods Producer **/
+
+Producer::Producer(GPIO_TypeDef *GPIOx , uint16_t GPIO_Pin)
+{
+	this->GPIO_Pin = GPIO_Pin ;
+	this->GPIOx = GPIOx ;
+	this->ProductId_u8 = this->sProducedId_u8 ;
+	this->sProducedId_u8 ++ ;
+	HAL_GPIO_WritePin(GPIOx , this->GPIO_Pin , GPIO_PIN_SET ) ;
+}
+
+Producer::~Producer(void)
+{
+	HAL_GPIO_WritePin(this->GPIOx , this->GPIO_Pin , GPIO_PIN_RESET ) ;
+}
+
+void Producer::InsertItem( CircularBuffer buff )
+{
+	buff.insert_item(this->ProductId_u8) ;
+}
