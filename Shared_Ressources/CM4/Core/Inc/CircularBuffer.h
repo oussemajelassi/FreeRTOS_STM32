17938672a8abce2f2b31485fb9a025f3eb8c44c0
oussemajelassi <oussjls57@gmail.com>
2023-10-19 16:44:45 +0100

    Fixed Dump old memory issue

--- Shared_Ressources/CM4/Core/Inc/CircularBuffer.h
+++ Shared_Ressources/CM4/Core/Inc/CircularBuffer.h
@@ -16,17 +16,17 @@
 class  CircularBuffer
 {
 private :
-	uint32_t size ;
 	uint32_t head_index ;
 	uint32_t tail_index ;
 	uint8_t * CircularBufferHandle ;
+	uint32_t GetFreeSpace ;
 	bool full ;
 
 public :
+	uint32_t size ;
 	CircularBuffer ( uint32_t size )   ;
 	uint8_t read_item( void ) ;
 	void insert_item ( uint8_t item ) ;
-	uint32_t GetFreeSpace ( void ) ;
 	void reset ( void ) ;
 	uint8_t * GetBuff ( void ) ;
 	~CircularBuffer (void) ;
